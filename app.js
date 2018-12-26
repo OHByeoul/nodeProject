b1fe1f4316f60e7993208b96a90c0afc3b5ebcb1
오한별 <suriessay@naver.com>
2018-12-27 04:46:36 +0900

    1. mongoDB 접속코드 작성

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const path = require('path'); // view위치잡아주려고 쓰는건가??
 
+//MongoDB 접속
+const mongoose = require('mongoose');
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', ()=>{
+    console.log('mongodb connect');
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/shoppingmall',{ useMongoClient: true });
+//
 const admin = require('./routes/admin');
 const app = express();
 const port = 3000;
